860fef54eac0a8605b687de4ee366bd4de5c3030
Julien Reiman <julienreiman@Juliens-MacBook-Pro-3.local>
2025-04-26 15:29:06 -0400

    Update app.py for Render deployment

--- app.py
+++ app.py
@@ -42,5 +42,6 @@ def upload_file():
 
     return render_template('index.html')
 
+# Important: Make Flask use host 0.0.0.0 and dynamic port from environment
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
